cdb1dee6a6b4e89b57d45c8c8465211902db9d1f
Alexandr <pushkin96@gmail.com>
2022-11-29 00:49:42 +0500

    выполнил task_8_HM

--- task_8_hw/Program.cs
+++ task_8_hw/Program.cs
@@ -1,6 +1,20 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
 Console.WriteLine("Введите положительное число для показа всех чётных чисел от 1 до выбранного числа");
-int a = Convert.ToInt32(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
 
-for ()
+if (N > 0)
+{
+    for (int i = 1; i < N; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write(i);
+            Console.Write(" ");
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Введено число меньшее или равное нулю");
+}
