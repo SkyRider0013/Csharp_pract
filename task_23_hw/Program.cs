db9e9b2c3726975407cdcb0df9db2360f1c4fe60
Alexandr <pushkin96@gmail.com>
2022-12-07 01:17:38 +0500

    выполнил задание 23

---
+++ task_23_hw/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите любое целое число для нахождения кубов всех чисел от 1 до заданного числа");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+if (number == 0)
+{
+    Console.WriteLine("Любая степень числа 0 всегда будет 0. Введите другое число");
+}
+else
+{
+    Console.Write("Таблица кубов до заданного ");
+    for (int i = 0; i < number + 1; i++)
+    {
+        double current = i;
+        current = Math.Pow(current, 3);
+        Console.Write(current + " ");
+    }
+}
