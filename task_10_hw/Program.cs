dde27cfce15cfdbd0dd1772089ffa1a644e61d75
Alexandr <pushkin96@gmail.com>
2022-12-06 16:06:56 +0500

    выполнил задачу 13

--- task_10_hw/Program.cs
+++ task_10_hw/Program.cs
@@ -1,14 +1,14 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-int[] array = new int[3];
+//int[] array = new int[3];
 
 Console.WriteLine("Введите трёхзначное число");
 int digit123 = Convert.ToInt32(Console.ReadLine());
-if (digit123 > 99 && digit123 < 1000)
+if (digit123 > 99 && digit123 < 1000) // оператор && даёт возможность совместить несколько условий для if
 {
     int digit12 = digit123 / 10;
     int digit2 = digit12%10;
-    Console.WriteLine($"в числе {digit123} вторая цифра {digit2}");
+    Console.WriteLine($"в числе {digit123} вторая цифра {digit2}");  // опертор $ даёт возможность вставлять ссылки на переменные для их вывода
 }
 else
 {
--- task_13_hw/Program.cs
+++ task_13_hw/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите любое целое число");
+string number = Console.ReadLine(); // запись всего, что было введено с клавиатуры, допустимо сразу в тип string
+// тип string сам по себе по сути является массивом
+int number1 = Convert.ToInt32(number); // вводим для проверки на отрицательное значение
+
+if (number1 > 0)
+{
+    if (number.Length > 2)
+    {
+        Console.WriteLine("В введённом числе третьей цифрой является " + number[2]);
+    }
+    else
+    {
+        Console.WriteLine("В введённом числе нет трёх цифр!");
+    }
+}
+else
+{
+    if (number.Length > 3)
+    {
+        Console.WriteLine("В введённом числе третьей цифрой является " + number[3]);
+    }
+    else
+    {
+        Console.WriteLine("В введённом числе нет трёх цифр!");
+    }
+}
