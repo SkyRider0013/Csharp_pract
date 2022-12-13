9c155a14d0f26bc5045738a0564f1801a1f1639b
Alexandr <pushkin96@gmail.com>
2022-12-13 17:14:40 +0500

    выполнил задание 29, попытался реализовать заполнение массива через функцию, но не понял, как мне сделать вывод результата из функции. Как итог, сделал без неё, но саму функцию закоментировал

---
+++ task_29_hw/Program.cs
@@ -0,0 +1,48 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.WriteLine("Введите размерность массива (для данной задачи он должен быть равен 8, но это не принципиально)");
+int length = Convert.ToInt32(Console.ReadLine());
+
+/*int WriteArray(int length)
+{
+    length = Math.Abs(length);
+    int[] array = new int[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine($"Введите {i} элемент массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array; ----- что тут нужно возвращать, чтобы данная функция заработала???
+}
+*/
+
+if (length == 0)
+{
+    Console.Write("Вы ввели 0 - массив пустой");
+}
+else if (length < 0)
+{
+    Console.WriteLine("Вы ввели отрицательное значени. В данной прогремме мы будем рассматривать это число по модулю");
+    length = Math.Abs(length);
+    int[] array = new int[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine($"Введите {i} элемент массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.Write("Получившийся массив равен: " + '[' + string.Join(", ", array) + ']');
+}
+else
+{
+    length = Math.Abs(length);
+    int[] array = new int[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine($"Введите {i} элемент массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.Write("Получившийся массив равен: " + '[' + string.Join(", ", array) + ']');
+}
