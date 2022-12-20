05f77b0270c811c2e834d7232fc868c4f8d45059
Alexandr <pushkin96@gmail.com>
2022-12-20 11:36:35 +0500

    выполнил задание 36

---
+++ task_36_hw/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+void PrintArray(int[] col)               
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write(col[position]);
+        Console.Write("; ");
+        position++;
+    }
+}
+int Interval(int[] arr)
+{
+    int counter = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 9 && arr[i] < 100)
+        {
+            counter = counter + 1;
+        }
+    }
+    return counter;
+}
+void FillArray(int[] collection)          
+{
+    int lenght = collection.Length;      
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(0, 1000); 
+        index++;
+    }
+}
+
+int[] array = new int[123];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Число значений массива, лежащих в интервале [10,99] равно {Interval(array)}");
